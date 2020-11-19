47948d472e0abde3b01057c5a71836ea57181781
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2020-11-19 08:47:47 +0100

    casi terminado el 13

---
+++ ej13_interval_atrapa_la_bola.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", main)
+const anchuraTablero = 600
+const alturaTablero = 400
+const anchuraBola = 30
+const alturaBola = 30
+const msIniciales = 1000
+const msDecremento = 25
+let tiempoInicial = 10
+let reloj
+let alarma
+let clicSobreBola
+
+function main() {
+    const tablero = document.querySelector("#tablero")
+    const bola = document.querySelector("#bola")
+
+    const velocidad = document.querySelector("#velocidad")
+    velocidad.textContent = msIniciales
+    const puntos = document.querySelector("#puntos")
+    puntos.textContent = 0
+    const tiempo = document.querySelector("#tiempo")
+    tiempo.textContent = tiempoInicial
+
+    tablero.style.width = anchuraTablero + "px"
+    tablero.style.height = alturaTablero + "px"
+    bola.style.width = anchuraBola + "px"
+    bola.style.height = alturaBola + "px"
+
+    // interval para controlar el tiempo real, los segundos que van pasando
+    reloj = setInterval(actualizaTiempo,1000)
+    // interval que actualiza la bola de posición cada X milisegundos
+    alarma = setInterval(cambiaPosicionBola,msIniciales)
+  
+    clicSobreBola = function() {
+        //Aumentar la puntuación
+        puntos.textContent = Number(puntos.textContent) + 1
+
+        //Que la posición se refresque inmediatamente
+        // (para evitar que se haga múltiples clics sobre la misma posicion)
+        cambiaPosicionBola()
+
+        //Aumentar la velocidad a la que cambia la bola de posición
+        clearInterval(alarma)
+        let nuevosMilisegundos = msIniciales - msDecremento * puntos.textContent
+        alarma = setInterval(cambiaPosicionBola,nuevosMilisegundos)
+        velocidad.textContent = nuevosMilisegundos
+    }
+    bola.addEventListener("click",clicSobreBola)
+}
+
+function cambiaPosicionBola(){
+    let nuevoTop = Math.floor( Math.random() * (alturaTablero - alturaBola) )
+    bola.style.top = nuevoTop + "px"
+    let nuevoLeft = Math.floor( Math.random() * (anchuraTablero - anchuraBola) )
+    bola.style.left = nuevoLeft + "px"
+}
+
+function actualizaTiempo(){
+    console.log(tiempo.textContent)
+    //mostrar que el tiempo restante ha bajado un segundo
+    tiempo.textContent = tiempo.textContent - 1
+
+    //si el tiempo se ha agotado, parar el juego (parar el tiempo y parar el clic)
+    if (tiempo.textContent == 0) {
+        clearInterval(reloj)
+        clearInterval(alarma)
+        bola.removeEventListener("click",clicSobreBola)
+    }
+}
