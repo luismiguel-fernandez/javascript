9365408c130fa612b2d88f3471f5193ede870949
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2020-11-26 10:09:09 +0100

    ej16 en construccion

--- ej16_form_multas.js
+++ ej16_form_multas.js
@@ -6,30 +6,38 @@ document.addEventListener("DOMContentLoaded", main)
 function main() {
     const form1 = document.querySelector("form")
     form1.addEventListener("submit",function(e){
+
         //impedir que el formulario se envíe al pulsar INTRO o 
 		// clicar en un boton SUBMIT dentro del FORM
         e.preventDefault()
+
+        //borrar posibles mensajes de error (LABEL) de envíos anterios
+        const nombreError = document.querySelector("#conductorError")
+        const edadError = document.querySelector("#edadError")
+        const tipoCarneError = document.querySelector("#tipoCarneError")
+        const tipoInfraccionError = document.querySelector("#tipoInfraccionError")
+        nombreError.textContent = ""
+        edadError.textContent = ""
+        tipoCarneError.textContent = ""
+        tipoInfraccionError.textContent = ""
+
         //comprobar campos
         let todoOK = true
         if ( !campoNombreOK() ) {
             todoOK = false
-            const nombreError = document.querySelector("#conductorError")
             nombreError.textContent = "No ha escrito un nombre válido"
         }
-        //if ( !campoDniOK() ) todoOK = false
+        if ( !campoDniOK() ) todoOK = false
         if ( !campoEdadOK() ) {
             todoOK = false
-            const edadError = document.querySelector("#edadError")
             edadError.textContent = "No ha escrito una edad válida"
         }
         if ( !campoTipoCarneOK() ) {
             todoOK = false
-            const tipoCarneError = document.querySelector("#tipoCarneError")
             tipoCarneError.textContent = "No ha seleccionado un tipo de carné"
         }
         if ( !campoTipoInfraccionOK() ) {
             todoOK = false
-            const tipoInfraccionError = document.querySelector("#tipoInfraccionError")
             tipoInfraccionError.textContent = "No ha seleccionado un tipo de infracción"
         }
 
@@ -49,7 +57,21 @@ function campoNombreOK() {
 }
 
 function campoDniOK() {
-
+    //comprobar DNI haciendo uso de expresiones regulares y la función test
+    const dni = document.querySelector("#dni").value
+    // que empieze por 8 dígitos decimales y acaba en una letra
+    // y opcionalmente podríamos aceptar guión medio
+    const patronDni = /^\d{8}-{0,1}[a-zA-Z]$/
+    //comprobación con función "test"
+    if (patronDni.test(dni)) {
+        //el dni que ha escrito el usuario cumple el patrón
+        console.log("el dni está OK")
+        return true
+    } else {
+        //el dni que ha escrito el usuario NO cumple el patrón
+        console.log("el dni NO cumple el patrón")
+        return false
+    }
 }
 
 function campoEdadOK() {
