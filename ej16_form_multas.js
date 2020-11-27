016c76e4296892ce554aae53416f94f77c192df7
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2020-11-27 13:02:07 +0100

    ej16 terminado

--- ej16_form_multas.js
+++ ej16_form_multas.js
@@ -13,6 +13,7 @@ function main() {
 
         //borrar posibles mensajes de error (LABEL) de envíos anterios
         const nombreError = document.querySelector("#conductorError")
+        const dniError = document.querySelector("#dniError")
         const edadError = document.querySelector("#edadError")
         const tipoCarneError = document.querySelector("#tipoCarneError")
         const tipoInfraccionError = document.querySelector("#tipoInfraccionError")
@@ -27,7 +28,20 @@ function main() {
             todoOK = false
             nombreError.textContent = "No ha escrito un nombre válido"
         }
-        if ( !campoDniOK() ) todoOK = false
+        switch ( campoDniOK() ) {
+            case 1:
+                //no hay que sacar ningún mensaje
+                dniError.textContent = ""
+                break 
+            case 2:
+                dniError.textContent = "El DNI introducido no cumple el patrón"
+                todoOK = false
+                break
+            case 3:
+                dniError.textContent = "La letra introducida no corresponde con el número"
+                todoOK = false
+                break
+        }
         if ( !campoEdadOK() ) {
             todoOK = false
             edadError.textContent = "No ha escrito una edad válida"
@@ -57,20 +71,32 @@ function campoNombreOK() {
 }
 
 function campoDniOK() {
+    //DEVOLVERÁ: 1 si todo OK
+    //           2 si lo que ha fallado es el patrón
+    //           3 si lo que ha fallado es la letra del DNI
+
     //comprobar DNI haciendo uso de expresiones regulares y la función test
     const dni = document.querySelector("#dni").value
     // que empieze por 8 dígitos decimales y acaba en una letra
     // y opcionalmente podríamos aceptar guión medio
-    const patronDni = /^\d{8}-{0,1}[a-zA-Z]$/
+    const patronDni = /^\d{8}[a-zA-Z]$/
     //comprobación con función "test"
     if (patronDni.test(dni)) {
         //el dni que ha escrito el usuario cumple el patrón
-        console.log("el dni está OK")
-        return true
+        //falta comprobar si la letra es la correcta para ese número
+        //Se separan los números de la letra
+        let letraDNI = dni.substring(8, 9).toUpperCase();
+        let numDNI = parseInt(dni.substring(0, 8));
+        //Se calcula la letra correspondiente al número
+        const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+        let letraCorrecta = letras[numDNI % 23];
+        if (letraDNI != letraCorrecta)
+            return 3
+        else
+            return 1
     } else {
         //el dni que ha escrito el usuario NO cumple el patrón
-        console.log("el dni NO cumple el patrón")
-        return false
+        return 2
     }
 }
 
